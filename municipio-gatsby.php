3d5835b649907ebd6a5d97c1f24995bf32096033
Cynthia Dao <cynthiamicheld@gmail.com>
2021-06-16 11:26:50 +0000

    style: auto-format all files [skip ci]

--- municipio-gatsby.php
+++ municipio-gatsby.php
@@ -7,11 +7,8 @@
  * Author:            Whitespace Dev
  */
 
-define('MUNICIPIO_GATSBY_PATH', dirname(__FILE__));
-define(
-  'MUNICIPIO_GATSBY_AUTOLOAD_PATH',
-  MUNICIPIO_GATSBY_PATH . '/autoload'
-);
+define("MUNICIPIO_GATSBY_PATH", dirname(__FILE__));
+define("MUNICIPIO_GATSBY_AUTOLOAD_PATH", MUNICIPIO_GATSBY_PATH . "/autoload");
 
 array_map(static function () {
   include_once func_get_args()[0];
