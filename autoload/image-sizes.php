2626a3e9fd8c4b07eb59fca31dac871292a830f8
Fredrik Johansson <jeanfredrik@gmail.com>
2021-06-16 15:01:29 +0200

    feat: add 16-by-9 image sizes

---
+++ autoload/image-sizes.php
@@ -0,0 +1,7 @@
+<?php
+
+// 16 by 9
+add_image_size('Wide large', 1920, 1080, true);
+add_image_size('Wide medium', 960, 540, true);
+add_image_size('Wide small', 480, 270, true);
+add_image_size('Wide tiny', 240, 135, true);
