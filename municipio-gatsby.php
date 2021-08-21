41f05b5c33fcf71e0077d4d8f0ac9048c5da0281
Fredrik Johansson <jeanfredrik@gmail.com>
2021-08-21 13:45:52 +0200

    fix: correctly load textdomain

--- municipio-gatsby.php
+++ municipio-gatsby.php
@@ -12,6 +12,12 @@
 define("MUNICIPIO_GATSBY_PATH", dirname(__FILE__));
 define("MUNICIPIO_GATSBY_AUTOLOAD_PATH", MUNICIPIO_GATSBY_PATH . "/autoload");
 
+add_action("init", function () {
+  $path = plugin_basename(dirname(__FILE__)) . "/languages";
+  load_plugin_textdomain("municipio-gatsby", false, $path);
+  load_muplugin_textdomain("municipio-gatsby", $path);
+});
+
 array_map(static function () {
   include_once func_get_args()[0];
 }, glob(MUNICIPIO_GATSBY_AUTOLOAD_PATH . "/*.php"));
