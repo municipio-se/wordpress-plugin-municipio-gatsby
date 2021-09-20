73fbd1e6df958b889263e3c14e49f1733b8b960a
Fredrik Johansson <jeanfredrik@gmail.com>
2021-09-20 16:50:08 +0200

    feat: Disable unimplemented options on Form module

---
+++ autoload/mod-form.php
@@ -0,0 +1,37 @@
+<?php
+
+add_action(
+  "admin_init",
+  function () {
+    if (function_exists("acf_remove_local_field")) {
+      // Conditional logic
+      acf_remove_local_field("field_59b69b14e2abf");
+      acf_remove_local_field("field_59b69ae3e2abd");
+      acf_remove_local_field("field_59b69ac9e2abb");
+      acf_remove_local_field("field_59b69ab2e2ab9");
+      acf_remove_local_field("field_59b69a93e2ab7");
+      acf_remove_local_field("field_59b69a7ae2ab5");
+      acf_remove_local_field("field_59b134beb61b0");
+      acf_remove_local_field("field_5a266baf943ad");
+      // Conditional field
+      acf_remove_local_field("field_59b69b16e2ac0");
+      acf_remove_local_field("field_59b69ae6e2abe");
+      acf_remove_local_field("field_59b69acbe2abc");
+      acf_remove_local_field("field_59b69ab5e2aba");
+      acf_remove_local_field("field_59b69a96e2ab8");
+      acf_remove_local_field("field_59b69a7de2ab6");
+      acf_remove_local_field("field_59b635765e86b");
+      acf_remove_local_field("field_5a266baf943ae");
+    }
+  },
+  20
+);
+
+// Display as
+add_filter("acf/load_field/key=field_58eb302883a68", function ($field) {
+  // Remove unimplemented field types
+  unset($field["layouts"]["58eccdd75ad90"]); // File upload
+  unset($field["layouts"]["5a266baf943ab"]); // Collapse
+
+  return $field;
+});
