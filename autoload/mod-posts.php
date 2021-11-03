88f3745de06f0bf6eeba9dc90fb509e434838f53
Fredrik Johansson <jeanfredrik@gmail.com>
2021-11-03 16:16:53 +0100

    feat!: re-add "Front End taxonomy filtering" field

--- autoload/mod-posts.php
+++ autoload/mod-posts.php
@@ -27,13 +27,25 @@ add_action(
 
       // Column values
       acf_remove_local_field("field_57625a3e188da");
-    }
-    if (function_exists("acf_remove_local_field_group")) {
-      // Data sorting
-      acf_remove_local_field_group("group_571dffc63090c");
 
       // Data filtering
-      acf_remove_local_field_group("group_571e045dd555d");
+      acf_remove_local_field("field_571e046536f0f"); // Taxonomy filter
+      acf_remove_local_field("field_571e048136f10"); // Taxonomy type
+      acf_remove_local_field("field_571e049636f11"); // Taxonomy value
+      acf_remove_local_field("field_571e04a736f12"); // Meta filter
+      acf_remove_local_field("field_571e04c736f13"); // Meta key
+      acf_remove_local_field("field_571e04da36f14"); // Meta value
+      // acf_remove_local_field('field_5af2f2e486366'); // Front End taxonomy filtering
+      acf_remove_local_field("field_5af2f64510be1"); // Front End Text search
+      acf_remove_local_field("field_5af2f68810be2"); // Front End Date range
+      acf_remove_local_field("field_5af2f6fb10be3"); // Front End Taxonomy
+      acf_remove_local_field("field_5b0d0c3f955b6"); // Front End Button text
+      acf_remove_local_field("field_5b0d103f8dc5a"); // Front End Hide date
+      acf_remove_local_field("field_5b0d10e78dc5b"); // Front End Display
+    }
+    if (function_exists("acf_remove_local_field_group")) {
+      // // Data sorting
+      acf_remove_local_field_group("group_571dffc63090c");
 
       // Taxonomy display
       acf_remove_local_field_group(
@@ -66,6 +78,32 @@ add_filter("acf/load_field/key=field_571dfd4c0d9d9", function ($field) {
 add_filter("acf/load_field/key=field_571e01e7f246c", function ($field) {
   // Remove unimplemented options
   unset($field["choices"]["title"]);
+
+  return $field;
+});
+
+// Front End taxonomy filtering
+add_filter("acf/load_field/key=field_5af2f2e486366", function ($field) {
+  $field["label"] = __("Allow user to filter results", "municipio_gatsby");
+
+  // XXX: This doesn’t really work because "Data source" is in another field group.
+  // $field["conditional_logic"] = [
+  //   [
+  //     [
+  //       "field" => "field_571dfaafe6984", // Data source
+  //       "operator" => "==",
+  //       "value" => "posttype",
+  //     ],
+  //   ],
+  // ];
+
+  // XXX: So we provide instructions instead.
+  $field["instructions"] = __(
+    "Only works if data source is set to post type",
+    "municipio_gatsby"
+  );
+
+  unset($field["conditional_logic"]);
 
   return $field;
 });
