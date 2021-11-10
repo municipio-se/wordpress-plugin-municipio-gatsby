f51058ab2b0b7d425ae8ee22e47049289b6c2da0
Mattias Hinderson <mattias@hinderson.com>
2021-11-10 13:50:41 +0000

    style: auto-format all files [skip ci]

--- autoload/mod-contacts.php
+++ autoload/mod-contacts.php
@@ -3,11 +3,11 @@
 // Remove unimplemented fields for the Contacts module
 add_action("init", function () {
   if (function_exists("acf_remove_local_field")) {
-     // Display mode
+    // Display mode
     acf_remove_local_field("field_5805e5dc1da44");
-     // Columns
+    // Columns
     acf_remove_local_field("field_5805e5dc1ddcd");
-     // Compact mode
+    // Compact mode
     acf_remove_local_field("field_5bffd2f45667f");
     // Display settings
     acf_remove_local_field("field_5bf7eec6ce764");
