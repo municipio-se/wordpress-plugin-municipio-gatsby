21121a4adb2c6f13dc96ead6b1e649488d789649
Mattias Hinderson <mattias@hinderson.com>
2022-03-10 15:06:36 +0000

    style: auto-format all files [skip ci]

--- municipio-gatsby.php
+++ municipio-gatsby.php
@@ -12,10 +12,17 @@
 define("MUNICIPIO_GATSBY_PLUGIN_FILE", __FILE__);
 define("MUNICIPIO_GATSBY_PATH", dirname(__FILE__));
 define("MUNICIPIO_GATSBY_AUTOLOAD_PATH", MUNICIPIO_GATSBY_PATH . "/autoload");
-define("MUNICIPIO_GATSBY_LANGUAGES_PATH", plugin_basename(dirname(__FILE__)) . "/languages");
+define(
+  "MUNICIPIO_GATSBY_LANGUAGES_PATH",
+  plugin_basename(dirname(__FILE__)) . "/languages"
+);
 
 add_action("plugins_loaded", function () {
-  load_plugin_textdomain("municipio-gatsby", false, MUNICIPIO_GATSBY_LANGUAGES_PATH);
+  load_plugin_textdomain(
+    "municipio-gatsby",
+    false,
+    MUNICIPIO_GATSBY_LANGUAGES_PATH
+  );
 });
 
 add_action("muplugins_loaded", function () {
