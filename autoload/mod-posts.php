7935ab290e50e7e6fb15c0853d7249fea3957789
Ruslana Logosha <63247495+RuslanaLogosha@users.noreply.github.com>
2022-10-31 17:48:50 +0100

    fix: add instructions to Link and Theme color for Manual input

--- autoload/mod-posts.php
+++ autoload/mod-posts.php
@@ -158,6 +158,7 @@ add_action(
       "name" => "link",
       "parent" => "field_576258d3110b0", // Data display
       "type" => "link",
+      'instructions' => 'Endast tillgängligt om visningsläge är kort eller block',
     ]);
   },
   20
