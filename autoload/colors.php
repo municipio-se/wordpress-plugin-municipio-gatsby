9551c079d3cb2f0fb4e183556aec2cf1de132034
Fredrik Johansson <jeanfredrik@gmail.com>
2023-06-07 15:41:17 +0200

    fix: Remove color field instructions

--- autoload/colors.php
+++ autoload/colors.php
@@ -120,10 +120,7 @@ function municipio_gatsby_get_theme_field($extra = []) {
     "name" => "theme",
     "type" => count($options) > 8 ? "select" : "radio",
     // "parent" => "", // Intentionally left out
-    "instructions" => __(
-      "Only available if display mode is cards or blocks",
-      "municipio-gatsby"
-    ),
+    "instructions" => "",
     "show_in_graphql" => true,
     "graphql_field_name" => "theme",
     "choices" => $options,
